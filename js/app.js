6ea32d74862bb0b134036b0232e124261f5dff56
Cannellespeculos <cjeanpierre@edenschool.fr>
2024-05-14 15:14:17 +0200

    show country cards and start select by region

---
+++ js/app.js
@@ -0,0 +1,68 @@
+const results = document.getElementById("results")
+const regions = document.getElementById("regions")
+const cards = document.getElementsByClassName("country-card")
+
+
+fetch("https://restcountries.com/v3.1/all?fields=name,capital,region,flags,population")
+.then((res) => res.json())
+.then((res) => {
+    console.log(res)
+
+    for (let i = 0; i < 50; i++) {
+        const element = res[i];
+        
+        // create the element for the cards
+        let div = document.createElement('div')
+        let h2 = document.createElement('h2')
+        let img = document.createElement('img')
+        let underdiv = document.createElement('div')
+        let ul = document.createElement('ul')
+        let populLi = document.createElement('li')
+        let regionLi = document.createElement('li')
+        let capitalLi = document.createElement('li')
+
+        // add text in the element
+        h2.textContent = element.name.official
+        populLi.textContent = "Population : " + element.population
+        regionLi.textContent = "Region : " + element.region
+        capitalLi.textContent = "Capital : " + element.capital[0]
+
+        // set attribute in img
+        img.setAttribute("src", element.flags.svg)
+        img.setAttribute("alt", element.flags.alt)
+
+        // add a class to element
+        div.className = "country-card"
+        div.setAttribute("id", element.region)
+        img.className = "country-flag"
+        underdiv.className = "country-details"
+
+        // append element
+        div.append(img)
+        underdiv.append(h2)
+        results.append(div)
+        div.append(underdiv)
+        underdiv.append(ul)        
+        ul.append(populLi)
+        ul.append(regionLi)
+        ul.append(capitalLi)        
+        
+    }
+
+    regions.addEventListener("change", (ev) => {
+        console.log(ev.target.value)
+        let e = ev.target.value 
+
+        for (let i = 0; i < cards.length; i++) {
+            const card = cards[i]
+            let element = cards[i].id;
+            element = element.toLowerCase()
+
+            if (element !== e) {
+                card.className += " erase"
+            }
+            
+        }
+    })
+    
+})
